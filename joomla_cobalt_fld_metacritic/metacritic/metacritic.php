a606d227b97de0281558d16934e82b62cee7fd89
Patrick Jones <patrick@patrickmjones.com>
2017-06-05 23:32:03 -0400

    Added caching to Metacritic field

--- joomla_cobalt_fld_metacritic/metacritic/metacritic.php
+++ joomla_cobalt_fld_metacritic/metacritic/metacritic.php
@@ -19,15 +19,30 @@ class JFormFieldCMetacritic extends CFormField
 	}
 	public function onRenderFull($record, $type, $section)
 	{
-        if(!empty($this->value)) {
-            $url = parse_url($this->value)['path'];
-            if(!empty($url)) {
-                $api = new MetacriticAPI($url);
-                $this->values = array();
-                $this->values['url'] = $this->value;
-                $this->values['score'] = $api->extractScore();
-            }
+        if(empty($this->value)) {
+            return; // eject - field is empty
         }
+
+        $url = parse_url($this->value)['path'];
+        if(!empty($url)) {
+            $this->values = array();
+            $this->values['url'] = $this->value;
+
+            $cache = JFactory::getCache('MetacriticCache', '');
+            $cache->setCaching(true);
+            $cache->setLifeTime(1800); // seconds: 30 mins
+            $cache_id = 'MetaCriticCache'.md5($this->value);
+            
+            $cached_score = $cache->get($cache_id);
+            if(!empty($cached_score)) {
+                $this->values['score'] = $cached_score;
+            }else{
+                $api = new MetacriticAPI($url);
+                $score = $api->extractScore();
+                $this->values['score'] = $score;
+                $cache->store($score, $cache_id);
+            }
+        }        
 		return $this->_display_output('full', $record, $type, $section);
 	}
 
